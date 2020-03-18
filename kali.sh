bb3c14256eecb667da5f359161bd46fb75d0e15a
Rasta Mouse <rasta@rastamouse.me>
2020-03-18 11:47:17 +0000

    CleanUp

--- kali.sh
+++ kali.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y update
-sudo apt install iptables-persistent netfilter-persistent
+sudo apt -y install iptables-persistent netfilter-persistent python-pip python3-pip
 
 sudo systemctl disable network-manager.service
 echo -en "\nauto eth0\niface eth0 inet dhcp\n\nauto eth1\niface eth1 inet static\n\taddress 192.168.152.100\n\tnetmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
@@ -10,7 +10,7 @@ sudo sysctl -w net.ipv4.ip_forward=1
 
 sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 sudo netfilter-persistent save
-sudo systemctl status netfilter-persistent
+sudo systemctl enable netfilter-persistent.service
 
 sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 
@@ -20,12 +20,14 @@ sudo apt -y update
 sudo apt -y install apt-transport-https
 sudo apt -y update
 sudo apt -y install dotnet-sdk-2.2
+rm packages-microsoft-prod.deb
 
 sudo git clone --recurse-submodules https://github.com/cobbr/Covenant /opt/Covenant
 sudo git clone https://github.com/rbsec/dnscan.git /opt/dnscan
 sudo git clone https://github.com/BishopFox/spoofcheck /opt/spoofcheck; cd /opt/spoofcheck; sudo pip install -r requirements.txt
 sudo git clone https://gist.github.com/superkojiman/11076951 /opt/namemash; sudo chmod +x /opt/namemash/namemash.py
 sudo git clone https://github.com/byt3bl33d3r/SprayingToolkit.git /opt/SprayingToolkit; cd /opt/SprayingToolkit; sudo pip3 install -r requirements.txt
+sudo gem install evil-winrm
 
 sudo systemctl enable ssh.service
 
