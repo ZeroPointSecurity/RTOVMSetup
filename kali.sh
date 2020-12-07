e2aeb205d70b66201232ef8e9a72cd75ecfe7de6
Rasta Mouse <rasta@rastamouse.me>
2020-12-07 14:01:29 +0000

    Update installation scripts

--- kali.sh
+++ kali.sh
@@ -12,8 +12,6 @@ sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 sudo netfilter-persistent save
 sudo systemctl enable netfilter-persistent.service
 
-sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
-
 wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt -y update
@@ -22,7 +20,7 @@ sudo apt -y update
 sudo apt -y install dotnet-sdk-3.1
 rm packages-microsoft-prod.deb
 
-sudo git clone --recurse-submodules https://github.com/cobbr/Covenant /opt/Covenant
+sudo git clone --recurse-submodules https://github.com/ZeroPointSecurity/Covenant.git /opt/Covenant
 sudo git clone https://github.com/rbsec/dnscan.git /opt/dnscan
 sudo git clone https://github.com/chinarulezzz/spoofcheck /opt/spoofcheck; cd /opt/spoofcheck; sudo pip3 install -r requirements.txt
 sudo git clone https://gist.github.com/superkojiman/11076951 /opt/namemash; sudo chmod +x /opt/namemash/namemash.py
