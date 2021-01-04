5f09336006f0ec6a891e600b58a20bf1457aad05
Rasta Mouse <rasta@rastamouse.me>
2021-01-04 10:29:56 +0000

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -6,7 +6,7 @@ sudo systemctl disable network-manager.service
 echo -en "\n\nauto eth0\niface eth0 inet dhcp\nauto eth1\niface eth1 inet static\n\taddress 192.168.152.100\n\tnetmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
 sudo service networking restart
 
-sudo sysctl -w net.ipv4.ip_forward=1
+sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 
 sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 sudo netfilter-persistent save
